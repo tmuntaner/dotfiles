b83be20db5c181910bbfdcbaed185c0cd1ea6718
Thomas Muntaner <thomas.muntaner@gmail.com>
2019-01-04 16:44:31 +0100

    Updated setup script more

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# vscode
-ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-
 # git
 ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+
+# gpg
+ln -sf ~/dotfiles/gnupg/gpg.conf ~/.gnupg/gpg.conf
+
+# vim
+ln -sf ~/vim/.vimrc ~/.vimrc
+
+# vscode
+ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+
+echo "Done"
