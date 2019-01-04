9bbdf917ffe79da1758a6cb05661ebb96137f66b
Thomas Muntaner <thomas.muntaner@gmail.com>
2019-01-04 16:36:10 +0100

    Added setup script

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 Personal configuration for various applications
 
-## VSCode
 
 ```
-ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+./setup.sh
 ```
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# vscode
+ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+
+# git
+ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
